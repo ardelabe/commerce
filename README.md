5132de7bab2d8449ce494b1bc8fb020e52158015
namaguederaz <ar.delattreabe@gmail.com>
2020-07-27 22:23:56 -0300

    models task concluded

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# commerce
+# COMMERCE
+
+# 0. INTRODUCTION
+
+# 1. MODELS
+Fist, I defined a string name to User and I went to python3 manage.py shell to add data and see some output - it was nice to see those things working.
+And "AbstractUser" is a predefined class that exists in django.contrib.auth.models that is VERY useful.
+When I tried to compile with a class with a ForeignKey before define the class witch this key as Primary I got an error of "not defined". 
+Quando cria o password pelo py, ele fica com os caracteres originais registrados - IMPORTANTE.
+Remember, that with $ sqlite filename.sql is possible to run queryes on terminal. 
+Dealing with data in python shell, the id doesn't show up until I saved the data in db. 
+With these observations, looks like the django-db relation is working fine. 
--- auctions/models.py
+++ auctions/models.py
@@ -1,8 +1,45 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.fields import DateTimeField
 
 # each time you change anything in auctions/models.py, you’ll need to first 
 # run python manage.py makemigrations and then python manage.py migrate to 
 # migrate those changes to your database.
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return f"id: {self.id} Name: {self.first_name} {self.last_name}"
+
+class Products(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=280)
+    def __str__(self):
+        return f"Product id: {self.id} - {self.title}"
+
+# one for auction listings (listagem de leiloes)
+class Auctions(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    is_active = models.BooleanField() # None by default
+    product_id = models.ForeignKey(Products, on_delete=models.PROTECT, related_name="auction_product")
+    initial_price = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return f"Auction id: {self.id} - Owner: {self.user_id} - active: {self.is_active} - price: {self.initial_price}"
+
+# one for bids (lances)
+class Bids(models.Model):
+    auction_id = models.ForeignKey(User, on_delete=models.PROTECT, related_name="auction-bid")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return f"in id {self.id}, User {self.id} gave a bid of {self.bid} on the product: {self.auction_id}"
+
+# and one for comments made on auction listings (comentarios na listagem)
+
+
+
+# class Passenger(models.Model):
+#     first = models.CharField(max_length=64)
+#     last = models.CharField(max_length=64)
+#     flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+#     def __str__(self):
+#         return f"{self.first} {self.last}"
